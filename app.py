a2df1e1851d65142ad8dce116e24a48b110c2e66
James Patrick <nebulasoftware1@gmail.com>
2026-09-10 12:52:54 +1000

    feat: setup basic db session and tested queries

--- app.py
+++ app.py
@@ -35,4 +35,18 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/shorten", status_code=status.HTTP_201_CREATED)
 async def create_short_url(payload: URLRequest):
-    return {"message": "URL received successfully", "target_url": payload.url}
+    db = SessionLocal()
+    try:
+        existing_url = db.query(URL).filter(URL.long_url == payload.url).first()
+
+        if existing_url:
+            return {"key": existing_url.short_code, "long_url": existing_url.long_url, "short_url": f"http://localhost:8000/{existing_url.short_code}"}
+        else:
+            new_url = URL(short_code="DUMMY123", long_url=payload.url)
+            db.add(new_url)
+            db.commit()
+            
+            return {"key": new_url.short_code, "long_url": new_url.long_url, "short_url": f"http://localhost:8000/{new_url.short_code}"}
+    finally:
+        db.close()
+        
