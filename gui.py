00757b9c7dff086aacd362029c2791e060b72a2e
James Patrick <nebulasoftware1@gmail.com>
2026-09-11 11:29:02 +1000

    feat: added del button logic in streamlit

--- gui.py
+++ gui.py
@@ -2,11 +2,28 @@ import streamlit as st
 import requests
 
 API_URL = "http://localhost:8000/shorten"
+BASE_URL = "http://localhost:8000"
 
 title = st.title("URL-Shortener")
 
-long_url = st.text_input("enter url")
+long_url = st.text_input("enter url (Full link (https:// required)")
 button = st.button("Shorten url")
+st.divider()
+code_to_del = st.text_input("Link to delete")
+
+del_button = st.button("Delete Link")
+
+if del_button:
+    if code_to_del:
+        try:
+            response = requests.delete(f"{BASE_URL}/{code_to_del}")
+            if response.ok:
+                st.success("Deletion successful")
+                st.write(f"{code_to_del} Deleted successfully")
+            else:
+                st.error("Failed to delete link")
+        except requests.exceptions.RequestException:
+            st.error("Backend is offline")
 
 if button:
     if long_url:
